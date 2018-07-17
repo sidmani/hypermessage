6fafca336606d98f759ef1603dd5300ce7eb4e37
Sid Mani <sid@sidmani.com>
2018-07-16 19:45:45 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// hypermessage
+// Copyright 2018 Sid Mani and released under the MIT license
+
+// Build an RFC-7230 compliant plaintext HTTP message
+module.exports.build = function(request) {
+  let str = `${r.method} ${r.endpoint} HTTP/${request.httpVersion || '1.1'}\n`;
+  if (r.headers) {
+    Object.keys(r.headers).forEach((key) => {
+      str += `${key}: ${r.headers[key]}\n`;
+    });
+  }
+  str += '\n';
+  str += r.body || '';
+  return str;
+};
